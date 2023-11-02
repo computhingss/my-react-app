538601bcea82ad2307c4970559d54b916b8cba3e
computhingss <84019210+computhingss@users.noreply.github.com>
2023-11-02 16:49:06 +0300

    son düzenlemeler yapıldı

--- src/App.js
+++ src/App.js
@@ -1,20 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
-import Card from './components/Card';
+import { Container, Grid, Stack, Button, Input, Textarea } from "@mantine/core";
+import "./App.css";
+import Card from "./components/Card";
+import { useState } from "react";
 
-function App() {
-  return(
-    <div>
-<p>Başlangıç</p>
-<div className='Cards'>
-<Card title="Dağ1" p="Açıklama 1"/>
-<Card title="Dağ2" p="Açıklama 2"/>
-<Card title="Dağ3" p="Açıklama 3"/>
-<Card title="Dağ4" p="Açıklama 4"/>
-
-</div>
-    </div>
+const App = () => {
+  const [title, setTitle] = useState("");
+  const [paragraf, setParagraph] = useState("");
+  const [list, setList] = useState([
+    {
+      id: 1,
+      title: "Dağ 1",
+      par: "Açıklama 1",
+    },
+    {
+      id: 2,
+      title: "Dağ 2",
+      par: "Açıklama 2",
+    },
+    {
+      id: 3,
+      title: "Dağ 3",
+      par: "Açıklama 3",
+    },
+  ]);
+  const click = () => {
+    setTitle("");
+    setParagraph("");
+    setList([
+      ...list,
+      {
+        id: 5,
+        title,
+        par: paragraf,
+      },
+    ]);
+  };
+  return (
+    <Container>
+      <h1>Kart oluşturma programı</h1>
+      <Stack>
+        <Input.Wrapper label="Başlık">
+          <Input
+            placeholder="Başlık yazınız"
+            width={400}
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </Input.Wrapper>
+        <Textarea
+          placeholder="Paragraf yazınız"
+          label="Paragraf"
+          value={paragraf}
+          onChange={(e) => setParagraph(e.target.value)}
+        />
+        <Button variant="outline" onClick={click}>
+          Kart oluştur
+        </Button>
+      </Stack>
+      <h2>Kartlar:</h2>
+      <Grid>
+        {list.map(({ par, title }, i) => (
+          <Grid.Col span={4} key={`index ${i}`}>
+            <Card
+              par={par}
+              title={title}
+              i={i}
+              click={() => {
+                let copyList = [...list];
+                copyList.splice(i, 1);
+                setList(copyList);
+              }}
+            />
+          </Grid.Col>
+        ))}
+      </Grid>
+    </Container>
   );
-}
+};
 
 export default App;
